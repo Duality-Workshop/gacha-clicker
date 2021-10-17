4d47bbf3962f911e0634f3612edbb8002609bc79
Linkyu <linkyu@users.noreply.github.com>
2021-10-17 08:31:23 +0200

    Change upgrade list from array to dictionnary

--- src/Manager.gd
+++ src/Manager.gd
@@ -17,7 +17,7 @@ var units
 var party = []
 
 # upgrades
-var upgrades = []
+var upgrades = {}
 const UPGRADE_RESOURCE = {
 	"Weapons": Upgrade.UpgradeResource.WEAPONS, 
 	"Scrolls": Upgrade.UpgradeResource.SCROLLS, 
@@ -62,12 +62,12 @@ func _ready():
 	# enum UpgradeResource {WEAPONS, SCROLLS, POTIONS, FOOD, BLUEPRINTS, GLOBAL}
 	# enum UpgradeScope {BASE, PERCENTAGE}
 	# enum UpgradeCategory {UC, UGB, UGP, URB, URP, CGB, CGP, CRB, CRP}
-	upgrades = [
+	upgrades = {
 		#func _init(type, target, resource, scope, power, name, description, flavour)
-		Upgrade.new(Upgrade.UpgradeType.DOMAIN, Upgrade.UpgradeTarget.CLICK, Upgrade.UpgradeResource.GLOBAL, Upgrade.UpgradeScope.PERCENTAGE, 2, {"Weapons": 256, "Scrolls": 100, "Blueprints": 500}, "Basic Water Pump", "Saluken needs to use two hands and a foot to push the lever, but it’ll be worth it for simple water access. – [i]Doubles Resources per pull.[/i]", "Splishy splashy"),
-		Upgrade.new(Upgrade.UpgradeType.DEDICATION, Upgrade.UpgradeTarget.UNIT, Upgrade.UpgradeResource.GLOBAL, Upgrade.UpgradeScope.BASE, 2, {"Food": 10}, "GachaFAQs Strategy Guides", "Somehow these walls of monospace text feel comforting, in addition to be pretty helpful. – [i]+2 base resource per call.[/i]", "Somebody has done the hard work before, take advantage of this."),
+		"Water Pump": Upgrade.new(Upgrade.UpgradeType.DOMAIN, Upgrade.UpgradeTarget.CLICK, Upgrade.UpgradeResource.GLOBAL, Upgrade.UpgradeScope.PERCENTAGE, 2, {"Weapons": 256, "Scrolls": 100, "Blueprints": 500}, "Basic Water Pump", "Saluken needs to use two hands and a foot to push the lever, but it’ll be worth it for simple water access. – [i]Doubles Resources per pull.[/i]", "Splishy splashy"),
+		"GachaFAQs": Upgrade.new(Upgrade.UpgradeType.DEDICATION, Upgrade.UpgradeTarget.UNIT, Upgrade.UpgradeResource.GLOBAL, Upgrade.UpgradeScope.BASE, 2, {"Food": 10}, "GachaFAQs Strategy Guides", "Somehow these walls of monospace text feel comforting, in addition to be pretty helpful. – [i]+2 base resource per call.[/i]", "Somebody has done the hard work before, take advantage of this."),
 		
-	]
+	}
 	
 	party_list = get_parent().get_node("Board").get_node("PartyList")
 	unit_list = get_parent().get_node("Board").get_node("UnitList")
@@ -100,8 +100,8 @@ func _ready():
 		#units["Antoinette"].rank = 5
 		
 		
-		for upgrade in upgrades:
-			upgrade.unlocked = true
+		for id in upgrades:
+			upgrades[id].unlocked = true
 			#upgrade.owned = true
 
 
@@ -187,13 +187,15 @@ func get_upgrade_category_bonus(category, resource=null):
 	var bonus = 0
 	if get_category_type(category) == Upgrade.UpgradeScope.BASE:
 		bonus = 0
-		for upgrade in upgrades:
+		for id in upgrades:
+			var upgrade = upgrades[id]
 			if upgrade.owned and upgrade.category == category:
 				if resource == null or upgrade.resource == resource:
 					bonus += upgrade.power
 	elif get_category_type(category) == Upgrade.UpgradeScope.PERCENTAGE:
 		bonus = 1
-		for upgrade in upgrades:
+		for id in upgrades:
+			var upgrade = upgrades[id]
 			if upgrade.owned and upgrade.category == category:
 				if resource == null or upgrade.resource == resource:
 					bonus *= upgrade.power
@@ -201,7 +203,8 @@ func get_upgrade_category_bonus(category, resource=null):
 
 func get_unlocked_upgrades(type):
 	var ups = []
-	for upgrade in upgrades:
+	for id in upgrades:
+		var upgrade = upgrades[id]
 		if upgrade.type == type and upgrade.unlocked and not upgrade.owned:
 			ups.append(upgrade)
 	return ups
