596d645c31de6dc692ce93ecec6309354d21860e
Linkyu <Linkyu@users.noreply.github.com>
2021-12-30 15:56:36 +0100

    Finish dialogic implementation

--- src/TestDialogic.gd
+++ src/TestDialogic.gd
@@ -5,7 +5,8 @@ var new_dialog
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	new_dialog = Dialogic.start('GLORIA_INTRO1_PART1')
+	new_dialog = Dialogic.start('GLORIA_INTRO1_PART2')
+	new_dialog.connect("timeline_end", self, "after_dialog")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +15,9 @@ func _ready() -> void:
 
 
 func _on_Button_pressed() -> void:
+	get_parent().get_node("AnimationPlayer").play("Fade")
 	add_child(new_dialog)
+
+
+func after_dialog(timeline_name) -> void:
+		get_parent().get_node("AnimationPlayer").play_backwards("Fade")
