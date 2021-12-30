ed0fbda02e96d87e2b3ec61dc4370ea9735aed69
Linkyu <Linkyu@users.noreply.github.com>
2021-12-30 10:40:06 +0100

    Add crystal limiter

--- src/CallCrystalButton.gd
+++ src/CallCrystalButton.gd
@@ -30,34 +30,41 @@ func _ready():
 
 
 func _on_CallCrystalButton_pressed():
-	clicks += 1
-	$AmountLabel.text = str(clicks)
-	var rng = RandomNumberGenerator.new()
-	if Manager.RANDOM: rng.randomize()
-	var roll = rng.randi_range(0, roll_sum)
-	var result
-	
-	for odd in roll_table:
-		if roll <= int(odd):
-			result = roll_table[odd]
-			break
-	
-	if result in Helper.RESOURCE_TYPE.values():
-		emit_signal("increase_resource", result, Manager.get_click_power(result))
+	if $Timer.time_left == 0:
+		$Timer.start()
+		disabled = true
+		clicks += 1
+		$AmountLabel.text = str(clicks)
+		var rng = RandomNumberGenerator.new()
+		if Manager.RANDOM: rng.randomize()
+		var roll = rng.randi_range(0, roll_sum)
+		var result
 		
-		var r_emitter = resource_particle_emitter.instance()
-		r_emitter.emitting = true
-		r_emitter.process_material.color = Helper.get_resource_color(result)
-		add_child(r_emitter)
+		for odd in roll_table:
+			if roll <= int(odd):
+				result = roll_table[odd]
+				break
 		
-		var s_emitter = spark_particle_emitter.instance()
-		s_emitter.position = get_local_mouse_position()
-		s_emitter.emitting = true
-		add_child(s_emitter)
-	else:
-		Manager.pull_random()
-		
-		var emitter = crystal_particle_emitter.instance()
-		emitter.position = get_local_mouse_position()
-		emitter.emitting = true
-		add_child(emitter)
+		if result in Helper.RESOURCE_TYPE.values():
+			emit_signal("increase_resource", result, Manager.get_click_power(result))
+			
+			var r_emitter = resource_particle_emitter.instance()
+			r_emitter.emitting = true
+			r_emitter.process_material.color = Helper.get_resource_color(result)
+			add_child(r_emitter)
+			
+			var s_emitter = spark_particle_emitter.instance()
+			s_emitter.position = get_local_mouse_position()
+			s_emitter.emitting = true
+			add_child(s_emitter)
+		else:
+			Manager.pull_random()
+			
+			var emitter = crystal_particle_emitter.instance()
+			emitter.position = get_local_mouse_position()
+			emitter.emitting = true
+			add_child(emitter)
+
+
+func _on_Timer_timeout() -> void:
+	disabled = false
