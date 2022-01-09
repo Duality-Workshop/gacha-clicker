d7899307709be1a585108d30a2f9c5b021ce03ce
Linkyu <Linkyu@users.noreply.github.com>
2022-01-09 12:07:56 +0100

    Separate unit and resource rolls

--- src/CallCrystalButton.gd
+++ src/CallCrystalButton.gd
@@ -5,6 +5,7 @@ var spark_particle_emitter = preload("res://Scenes/SparkParticle.tscn")
 var crystal_particle_emitter = preload("res://Scenes/CrystalParticle.tscn")
 
 var roll_odds = {}
+var unit_odds = 0.025
 var roll_sum
 var roll_table = {}
 
@@ -18,8 +19,7 @@ func _ready():
 		Helper.RESOURCE_TYPE.SCROLLS: 100,
 		Helper.RESOURCE_TYPE.POTIONS: 100,
 		Helper.RESOURCE_TYPE.FOOD: 100,
-		Helper.RESOURCE_TYPE.BLUEPRINTS: 10,
-		"Unit": 1
+		Helper.RESOURCE_TYPE.BLUEPRINTS: 10
 	}
 	
 	var s = 0
@@ -38,6 +38,7 @@ func _on_CallCrystalButton_pressed():
 		var rng = RandomNumberGenerator.new()
 		if Manager.RANDOM: rng.randomize()
 		var roll = rng.randi_range(0, roll_sum)
+		var unit_roll = rng.randf()
 		var result
 		
 		for odd in roll_table:
@@ -45,20 +46,21 @@ func _on_CallCrystalButton_pressed():
 				result = roll_table[odd]
 				break
 		
-		if result in Helper.RESOURCE_TYPE.values():
-			emit_signal("increase_resource", result, Manager.get_click_power(result))
-			
-			var r_emitter = resource_particle_emitter.instance()
-			r_emitter.emitting = true
-			r_emitter.process_material.color = Helper.get_resource_color(result)
-			r_emitter.process_material.emission_box_extents = Vector3(rect_size.x / 2, rect_size.y / 2, 0)
-			add_child(r_emitter)
-			
-			var s_emitter = spark_particle_emitter.instance()
-			s_emitter.position = get_local_mouse_position()
-			s_emitter.emitting = true
-			add_child(s_emitter)
-		else:
+		# Add resource
+		emit_signal("increase_resource", result, Manager.get_click_power(result))
+		var r_emitter = resource_particle_emitter.instance()
+		r_emitter.emitting = true
+		r_emitter.process_material.color = Helper.get_resource_color(result)
+		r_emitter.process_material.emission_box_extents = Vector3(rect_size.x / 2, rect_size.y / 2, 0)
+		add_child(r_emitter)
+		
+		var s_emitter = spark_particle_emitter.instance()
+		s_emitter.position = get_local_mouse_position()
+		s_emitter.emitting = true
+		add_child(s_emitter)
+		
+		# Add unit
+		if unit_roll <= unit_odds:
 			Manager.pull_random()
 			
 			var emitter = crystal_particle_emitter.instance()
